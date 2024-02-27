1970a834cd616b7dc1eeccc180ea346ad26a9b35
Adriana Villela <indycomp0t1@gmail.com>
2024-02-27 18:46:38 -0500

    Clean up example

--- README.md
+++ README.md
@@ -23,10 +23,28 @@ Build Docker image and deploy to KinD
 ./src/scripts/03-build-and-deploy-images.sh
 ```
 
+Verify that images are in KinD:
+
+```bash
+docker exec -it otel-target-allocator-talk-control-plane crictl images | grep target-allocator
+```
+
+Reference [here](https://kind.sigs.k8s.io/docs/user/quick-start/#loading-an-image-into-your-cluster).
+
 ### 3- Kubernetes deployment
 
 Deploy Kubernetes resources
 
 ```bash
 ./src/scripts/04-deploy-resources.sh
+```
+
+Check Collector logs:
+
+```bash
+# Option 1
+kubectl logs -l app.kubernetes.io/component=opentelemetry-collector -n opentelemetry --follow
+
+# Option 2
+kubectl logs otelcol-collector-0 -n opentelemetry | grep "Name:" | sort | uniq | xsel -b
 ```
--- src/scripts/02-k8s-setup.sh
+++ src/scripts/02-k8s-setup.sh
@@ -9,14 +9,14 @@ helm install prometheus prometheus-community/kube-prometheus-stack
 # kubectl --context kind-otel-target-allocator-talk apply -f src/resources/service-monitor.yml
 # kubectl --context kind-otel-target-allocator-talk apply -f src/resources/pod-monitor.yml
 
-sleep 30
+sleep 35
 
 # Install cert-manager
 # kubectl --context kind-otel-target-allocator-talk apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.3/cert-manager.yaml
 kubectl --context kind-otel-target-allocator-talk apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.10.0/cert-manager.yaml
 
 
-sleep 30
+sleep 35
 
 # Install operator
 # kubectl --context kind-otel-target-allocator-talk apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/download/v0.94.0/opentelemetry-operator.yaml
