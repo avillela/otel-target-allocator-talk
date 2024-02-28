7d0b4916981fb602935d9f4d3b126c8c520c51ee
Adriana Villela <indycomp0t1@gmail.com>
2024-02-28 12:23:08 -0500

    Update k8s setup script to include just podmonitor and servicemonitor CRs

--- src/scripts/02-k8s-setup.sh
+++ src/scripts/02-k8s-setup.sh
@@ -1,23 +1,21 @@
 #! /bin/bash
 
-# Install Prometheus Operator
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm install prometheus prometheus-community/kube-prometheus-stack
+# Install Prometheus Operator (overkill)
+# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+# helm repo update
+# helm install prometheus prometheus-community/kube-prometheus-stack
 
-# Install ServiceMonitor and PodMonitor
-# Find version of helm charts by running helm list
-# kubectl --context kind-otel-target-allocator-talk apply -f https://raw.githubusercontent.com/prometheus-community/helm-charts/kube-prometheus-stack-56.14.0/charts/kube-prometheus-stack/crds/crd-podmonitors.yaml
-# kubectl --context kind-otel-target-allocator-talk apply -f https://raw.githubusercontent.com/prometheus-community/helm-charts/kube-prometheus-stack-56.14.0/charts/kube-prometheus-stack/crds/crd-servicemonitors.yaml
-
-# kubectl --context kind-otel-target-allocator-talk apply -f src/resources/service-monitor.yml
-# kubectl --context kind-otel-target-allocator-talk apply -f src/resources/pod-monitor.yml
+# Install Just ServiceMonitor and PodMonitor
+# NOTE: This GH issue put me on the right track: https://github.com/open-telemetry/opentelemetry-operator/issues/1811#issuecomment-1584128371
+kubectl --context kind-otel-target-allocator-talk apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.2/example/prometheus-operator-crd/monitoring.coreos.com_servicemonitors.yaml
+kubectl --context kind-otel-target-allocator-talk apply -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.71.2/example/prometheus-operator-crd/monitoring.coreos.com_podmonitors.yaml
 
 sleep 35
 
 # Install cert-manager
 kubectl --context kind-otel-target-allocator-talk apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.3/cert-manager.yaml
 
+# Sometimes it takes a while for cert-manager pods to come up
 sleep 90
 
 # Install operator
