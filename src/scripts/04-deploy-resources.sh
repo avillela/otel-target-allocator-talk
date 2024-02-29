7fe4a8845f70529bf7b002be35568ed60029c708
Adriana Villela <indycomp0t1@gmail.com>
2024-02-28 19:06:40 -0500

    Update LS config so that token isn't exposed

--- src/scripts/04-deploy-resources.sh
+++ src/scripts/04-deploy-resources.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
 kubectl create ns opentelemetry
+kubectl apply -f src/resources/00-secret.yml
 kubectl apply -f src/resources/01-rbac.yml
 kubectl apply -f src/resources/02-otel-collector.yml
 kubectl apply -f src/resources/03-instrumentation.yml
 kubectl apply -f src/resources/04-service-monitor.yml
 kubectl apply -f src/resources/05-python-client.yml
 kubectl apply -f src/resources/06-python-server.yml
+# kubectl apply -f src/resources/07-k8s-metrics.yml
