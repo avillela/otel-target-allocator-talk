8986b459417debdf098b3a92c33746791f769d93
Adriana Villela <indycomp0t1@gmail.com>
2024-05-06 13:06:27 -0400

    Add PodMonitor example

--- src/scripts/04-deploy-resources-ls-backend.sh
+++ src/scripts/04-deploy-resources-ls-backend.sh
@@ -6,7 +6,7 @@ kubectl apply -f src/resources/01-rbac.yml
 kubectl apply -f src/resources/02-otel-collector-ls.yml
 kubectl apply -f src/resources/03-instrumentation.yml
 kubectl apply -f src/resources/04-service-monitor.yml
+kubectl apply -f src/resources/04a-pod-monitor.yml
 kubectl apply -f src/resources/05-python-client.yml
 kubectl apply -f src/resources/06-python-server.yml
-# kubectl apply -f src/resources/07-k8s-metrics.yml
 kubectl apply -f src/resources/08-python-app.yml
