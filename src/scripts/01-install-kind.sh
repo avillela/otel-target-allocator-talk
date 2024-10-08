135e5ff769ee8f1179823b2d7af3f5c0b77b68e6
Adriana Villela <50256412+avillela@users.noreply.github.com>
2024-10-08 16:16:32 -0400

    Update KinD version for amd64 (#11)

--- src/scripts/01-install-kind.sh
+++ src/scripts/01-install-kind.sh
@@ -5,7 +5,7 @@
 # ---------------------------
 
 # Install KinD
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-amd64
 [ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-arm64
 
 chmod +x ./kind
