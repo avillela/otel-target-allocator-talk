bc014e1757528afe7fb434f0eddb125e8ca1a4e9
Adriana Villela <50256412+avillela@users.noreply.github.com>
2024-10-15 17:29:52 -0400

    Workaround for KinD cluster failing to create in GH codespaces (#12)

--- src/scripts/01-install-kind.sh
+++ src/scripts/01-install-kind.sh
@@ -12,7 +12,8 @@ chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 kind --version
 
-# Create KinD cluster
+# Create KinD cluster. See the following GitHub issue for more on why I had to resort to manually creating the docker network: https://github.com/kubernetes-sigs/kind/issues/3748
+docker network create -d=bridge -o com.docker.network.bridge.enable_ip_masquerade=true -o com.docker.network.driver.mtu=1500 --subnet fc00:f853:ccd:e793::/64 kind
 kind create cluster --name otel-target-allocator-talk
 
 # Install kubectl
