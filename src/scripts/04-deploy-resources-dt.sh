d49496b86776730466f4b5208c32951c494ed954
Adriana Villela <indycomp0t1@gmail.com>
2024-12-05 21:00:06 +0000

    Add dynatrace configs

--- src/scripts/04-deploy-resources-dt.sh
+++ src/scripts/04-deploy-resources-dt.sh
@@ -1,18 +1,9 @@
 #! /bin/bash
 
 kubectl create ns opentelemetry
-
-if [ $# -eq 0 ]; then
-    SUFFIX=""
-    echo "** No arguments supplied **"
-else
-    SUFFIX="-$1"
-    echo "** Suffix is ${SUFFIX} **"
-    kubectl apply -f src/resources/00-secret${SUFFIX}.yml
-fi
-
+kubectl apply -f src/resources/00-secret-dt.yml
 kubectl apply -f src/resources/01-rbac.yml
-kubectl apply -f src/resources/02-otel-collector${SUFFIX}.yml
+kubectl apply -f src/resources/02-otel-collector.yml
 kubectl apply -f src/resources/03-instrumentation.yml
 kubectl apply -f src/resources/04-service-monitor.yml
 kubectl apply -f src/resources/04a-pod-monitor.yml
--- src/scripts/04-deploy-resources-ls-backend.sh
+++ src/scripts/04-deploy-resources-ls-backend.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 kubectl create ns opentelemetry
-kubectl apply -f src/resources/00-secret.yml
+kubectl apply -f src/resources/00-secret-ls.yml
 kubectl apply -f src/resources/01-rbac.yml
 kubectl apply -f src/resources/02-otel-collector-ls.yml
 kubectl apply -f src/resources/03-instrumentation.yml
@@ -10,3 +10,4 @@ kubectl apply -f src/resources/04a-pod-monitor.yml
 kubectl apply -f src/resources/05-python-client.yml
 kubectl apply -f src/resources/06-python-server.yml
 kubectl apply -f src/resources/08-python-app.yml
+kubectl apply -f src/resources/09-jaeger.yml
