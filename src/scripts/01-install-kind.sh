fab7626dbd8119857978a8cad173bff3bc5a4001
Adriana Villela <indycomp0t1@gmail.com>
2024-12-06 17:24:17 +0000

    Cleaning up scripts

--- src/scripts/01-install-kind.sh
+++ src/scripts/01-install-kind.sh
@@ -4,9 +4,21 @@
 # -- Kubernetes installation
 # ---------------------------
 
+# Get distro information
+if [ $(uname -m) = x86_64 ]; then
+    DISTRO="amd64"
+elif [ $(uname -m) = aarch64 ]; then
+    DISTRO="arm64"
+fi
+
+echo "Distro is ${DISTRO}"
+
+
 # Install KinD
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-amd64
-[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-arm64
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-${DISTRO}
+
+# [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-amd64
+# [ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-arm64
 
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
@@ -17,8 +29,9 @@ docker network create -d=bridge -o com.docker.network.bridge.enable_ip_masquerad
 kind create cluster --name otel-target-allocator-talk
 
 # Install kubectl
-[ $(uname -m) = x86_64 ] && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-[ $(uname -m) = aarch64 ] && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${DISTRO}/kubectl"
+# [ $(uname -m) = x86_64 ] && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# [ $(uname -m) = aarch64 ] && curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
 
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
